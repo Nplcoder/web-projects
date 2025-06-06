fa36bcb48bd10ed9a69d938d6452b9d3c0ab625c
Bikash Moktan <moktan.bi@gmail.com>
2025-06-06 21:51:49 +0400

    Weight Converter

---
+++ Weight-Converter/script.js
@@ -0,0 +1,26 @@
+const inputElem = document.querySelector('#input')
+const errorElem = document.querySelector('#error')
+const resultElem = document.querySelector('#result')
+
+let errorTime;
+let resultTime;
+
+function updateResults(){
+    if(inputElem.value <= 0 || isNaN(inputElem.value)){
+        errorElem.innerText = "Please enter a valid number!";
+        clearTimeout(errorTime)
+        errorTime = setTimeout(() => {
+            errorElem.innerText = "";
+            inputElem.value = '';
+        },2000)
+    }else{
+        resultElem.innerText = (+inputElem.value / 2.2).toFixed(2);
+        clearTimeout(resultTime);
+        resultTime = setTimeout(() => {
+            resultElem.innerText = '';
+            inputElem.value = '';
+        },2000);
+    }
+}
+
+inputElem.addEventListener("input", updateResults)
