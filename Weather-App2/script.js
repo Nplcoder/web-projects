d1249ed4786087e1a13bd6eb043d158ed8028357
Bikash Moktan <moktan.bi@gmail.com>
2025-06-24 21:51:57 +0400

    Update: Web App 2

---
+++ Weather-App2/script.js
@@ -0,0 +1,6 @@
+const apiKey = "f54abaee51a9505b48a4f1802abdb88f";
+
+const weatherData = document.querySelector("#Weather-data");
+const cityInputELe = document.querySelector("#city-input");
+const formEle = document.querySelector("form");
+
