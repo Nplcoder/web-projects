7c6cdf12003dbe4972d62a8bed048da0f4a29c4a
Bikash Moktan <moktan.bi@gmail.com>
2025-06-25 21:20:00 +0400

    JS Api updates

--- Weather-App2/script.js
+++ Weather-App2/script.js
@@ -4,3 +4,35 @@ const weatherData = document.querySelector("#Weather-data");
 const cityInputELe = document.querySelector("#city-input");
 const formEle = document.querySelector("form");
 
+formEle.addEventListener('submit', (event) =>{
+    event.preventDefault();
+    const cityValue = cityInputELe.value
+    console.log(cityValue)
+    getWeatherData(cityValue);
+});
+
+async function getWeatherData(cityValue){
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`)
+
+        if(!response.ok){
+            throw new Error("Network response was not ok")
+        }
+
+        const data = await response.json()
+        console.log(data)
+
+        const temperature = Math.round(data.main.temp)
+        const description = data.weather[0].description
+        const icon = data.weather[0].icon
+        const details = [
+            `Feels like: ${Math.round(data.main.feels_like)}`,
+            `Humidity: ${data.main.humidity}`,
+            `Wind speed: ${data.wind.speed}`,
+        ]
+
+        weatherData.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`
+    }catch(error){
+
+    }
+}
