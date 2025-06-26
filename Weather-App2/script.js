935cc916bb7adcec80c8ad98d261acca2941ae16
Bikash Moktan <moktan.bi@gmail.com>
2025-06-26 21:51:12 +0400

    Project Completed

--- Weather-App2/script.js
+++ Weather-App2/script.js
@@ -27,12 +27,21 @@ async function getWeatherData(cityValue){
         const icon = data.weather[0].icon
         const details = [
             `Feels like: ${Math.round(data.main.feels_like)}`,
-            `Humidity: ${data.main.humidity}`,
-            `Wind speed: ${data.wind.speed}`,
+            `Humidity: ${data.main.humidity}%`,
+            `Wind speed: ${data.wind.speed} m/s`,
         ]
 
-        weatherData.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`
-    }catch(error){
+        weatherData.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
+        weatherData.querySelector(".temperature").textContent = `${temperature}°C`;
+        weatherData.querySelector('.description').textContent = description;
 
+        weatherData.querySelector('.details').innerHTML = details.map((details) => `<div>${details}</div>`).join("");
+
+    }catch(error){
+        weatherData.querySelector(".icon").innerHTML = ``;
+        weatherData.querySelector(".temperature").textContent = ``;
+        weatherData.querySelector('.description').textContent = "An Error Happened, Please try again later";
+
+        weatherData.querySelector('.details').innerHTML = details.map((details) => ``).join("");
     }
 }
